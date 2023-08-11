03c70dd58e478be40719857270790ad4afac1434
Dulon <141568779+Dulon23@users.noreply.github.com>
2023-08-11 23:55:40 +0600

    Update solution1.js

--- solution1.js
+++ solution1.js
@@ -1,3 +1,9 @@
+/* 1) javascript For in loop
+ Write a JavaScript function named printObjectProperties that takes 
+ an object as an argument and uses the for...in loop to print all the
+ properties and their values in the following format: "property: value".
+ If the object is empty, print "Object is empty". */
+
 function printObjectProperties(obj) {
   if (Object.keys(obj).length === 0) {
     document.write("Object is empty");

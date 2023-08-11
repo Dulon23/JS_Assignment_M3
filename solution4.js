c8d7b7dcc1224895152e52198f9743622ffb4f39
Dulon <141568779+Dulon23@users.noreply.github.com>
2023-08-12 00:24:12 +0600

    Create solution4.js

---
+++ solution4.js
@@ -0,0 +1,7 @@
+/*
+4) javascript arrow function
+Write a JavaScript arrow function named calculateSquare that 
+takes a number as an argument and returns the square of that number.
+*/
+const calculateSquare = (number) => number * number;
+console.log(calculateSquare(5));

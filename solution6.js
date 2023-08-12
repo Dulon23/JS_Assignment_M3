7efeed8cd899adbbcf6a7787645ef5368bf65b51
Dulon <141568779+Dulon23@users.noreply.github.com>
2023-08-12 14:51:21 +0600

    Create solution6.js

---
+++ solution6.js
@@ -0,0 +1,11 @@
+/*
+Create a JavaScript class named MathUtility with an ES6 static method called multiply. 
+The multiply method should take two parameters and return their product.
+*/
+
+class MathUtility {
+  static multiply(a, b) {
+    return a * b;
+  }
+}
+console.log(MathUtility.multiply(5, 3));
